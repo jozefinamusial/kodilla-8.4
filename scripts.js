419fb4a5d1d747a06786a383b9ffbb75ce6f437d
Weronika Musial <jozefinamusial@gmail.com>
2017-03-10 00:40:05 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var a = 10,
+    h = 6,
+    triangleArea;
+    
+triangleArea = a * h / 2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
